216ac54863b3d4748521f3a898f09a8e9c8c7dda
Michael Korostelev <korostelevm@gmail.com>
2021-07-09 20:46:42 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 module.exports.all = (req,res) => {
   res.json({
-    Hello: ['Hola', 'Hggggello', 'Привет', '你好'],
+    Hello: ['Hola', 'Hggggjjjhhhhhhhhghfgggello', 'Привет', '你好'],
     World: '🌎',
     at: new Date().toISOString()
   }
